ea9fc592da6c10520aaf9dc3e88893e635f7bd62
ramunettam <ramunettam@gmail.com>
2024-04-22 15:57:29 +0200

    exp shell

--- frontend.sh
+++ frontend.sh
@@ -34,7 +34,7 @@ systemctl enable nginx &>>$LOGFILE
 VALIDATE $? "Enabling nginx"
 
 systemctl start nginx &>>$LOGFILE
-VALIDATE $? "starting nginx"
+VALIDATE $? "Starting nginx"
 
 rm -rf /usr/share/nginx/html/* &>>$LOGFILE
 VALIDATE $? "Removing existing content"
